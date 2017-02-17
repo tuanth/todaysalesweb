1ae0e3d20a7fe73f70d07d1b738e20f5776ec216
tuanth <huutuanfpt@gmail.com>
2017-02-17 08:08:19 +0700

    update index and add new file requiments

--- index.py
+++ index.py
@@ -5,11 +5,14 @@ import json
 import sys
 import datetime
 import boto3
-
+from path import Path
 """File name is deals_ + datetime with YearMonthDay"""
 daystr = datetime.date.today().strftime('%Y%m%d')
 filename = 'deals_'+daystr+'.jl'
 rootFolder = 'C:\crawlData\\'
+oldFiles = Path(rootFolder).files('*.jl')
+oldFiles[0].remove()
+print('Removed {} file').format(oldFiles[0])
 fullFilePath = rootFolder+filename
 """Download deals from S3"""
 session = boto3.Session(profile_name='indexingProf')
@@ -19,7 +22,7 @@ s3_client.download_file('home-deals', 'deals/'+filename, fullFilePath)
 
 # ixDirectory = 'indexed_'+daystr
 ixDirectory = rootFolder + 'indexed'
-dealSchema = Schema(title=TEXT(stored=True),img=ID(stored=True),link=TEXT(stored=True))
+dealSchema = Schema(title=TEXT(stored=True),img=ID(stored=True),link=TEXT(stored=True),price=ID(stored=True))
 if not os.path.exists(ixDirectory):
     os.mkdir(ixDirectory)
 ix = create_in(ixDirectory,dealSchema)
@@ -33,7 +36,8 @@ with open(fullFilePath) as file:
 			title = lineData['title']
 			img = lineData['img']
 			link = lineData['link']
-			writer.add_document(title=title, img=img, link=link)
+			price = lineData['prices']['pSale']
+			writer.add_document(title=title, img=img, link=link, price=price)
 		except ValueError as e:
 			continue	
 	writer.commit()
